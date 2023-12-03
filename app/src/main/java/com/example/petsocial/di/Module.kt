44c936218b93fe1dc6ccd92a7343d8c38c3e5490
volkan kelleci <kellecivolkan@gmail.com>
2023-12-03 19:49:50 +0300

    added, Dependency injection

--- app/src/main/java/com/example/petsocial/di/Module.kt
+++ app/src/main/java/com/example/petsocial/di/Module.kt
@@ -1,13 +1,21 @@
 package com.example.petsocial.di
 
+import android.content.Context
 import androidx.core.view.MenuHost
 import androidx.fragment.app.FragmentActivity
-import com.example.petsocial.CustomFragmentFactory
+import androidx.room.Room
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.petsocial.R
 import com.example.petsocial.feature.login.LoginRepositoryImpl
+import com.example.petsocial.feature.profile.ProfileRepositoryImpl
 import com.example.petsocial.feature.registiration.RegistirationRepositoryImpl
+import com.example.petsocial.room.UserDatabase
+import com.example.petsocial.room.UserInfoDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -15,6 +23,15 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 @Module
 object Module {
+
+    @Singleton
+    @Provides
+    fun injectRoomDatabase(@ApplicationContext context: Context) = Room.databaseBuilder(context, UserDatabase::class.java, "users").allowMainThreadQueries().build()
+
+    @Singleton
+    @Provides
+    fun injectDao(database: UserDatabase) = database.userInfoDao()
+
     @Provides
     @Singleton
     fun provideMenuHost(activity: FragmentActivity): MenuHost {
@@ -23,19 +40,29 @@ object Module {
 
     @Provides
     @Singleton
-    fun provideCustomFragmentFactory( ): CustomFragmentFactory {
-        return CustomFragmentFactory()
-    }
-    @Provides
-    @Singleton
     fun provideSignUpRepository(): RegistirationRepositoryImpl {
         return RegistirationRepositoryImpl()
     }
+
     @Provides
     @Singleton
-    fun provideSignInRepository(): LoginRepositoryImpl {
-        return LoginRepositoryImpl()
+    fun provideSignInRepository(userInfoDao: UserInfoDao): LoginRepositoryImpl {
+        return LoginRepositoryImpl(userInfoDao)
     }
 
+    @Provides
+    @Singleton
+    fun provideProfileRepository(userInfoDao: UserInfoDao) : ProfileRepositoryImpl {
+        return ProfileRepositoryImpl(userInfoDao)
+    }
+
+
+    @Singleton
+    @Provides
+    fun injectGlide(@ApplicationContext context: Context) = Glide
+        .with(context).setDefaultRequestOptions(
+            RequestOptions().placeholder(R.drawable.ic_launcher_foreground)
+                .error(R.drawable.ic_launcher_foreground)
+        )
 
 }
