09840bb16584df0dea2e81697a0ce7c761a13b2b
volkan kelleci <kellecivolkan@gmail.com>
2023-12-09 20:08:50 +0300

    Fixed,UI

---
+++ app/src/main/java/com/example/petsocial/room/UserDatabase.kt
@@ -0,0 +1,9 @@
+package com.example.petsocial.room
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [UserInfo::class], version = 1, exportSchema = true)
+abstract class UserDatabase:RoomDatabase() {
+    abstract fun userInfoDao():UserInfoDao
+}
