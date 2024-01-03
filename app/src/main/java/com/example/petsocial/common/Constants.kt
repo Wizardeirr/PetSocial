b6951211bbf059596ba6b2597bd2bdc92427985b
volkan kelleci <kellecivolkan@gmail.com>
2024-01-03 13:31:57 +0300

    Added, Constants

---
+++ app/src/main/java/com/example/petsocial/common/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.petsocial.common
+
+import com.example.petsocial.R
+
+object Constants {
+    const val cat= R.string.cat
+    const val dog=R.string.dog
+    const val yes=R.string.yes
+    const val no=R.string.no
+
+
+}
