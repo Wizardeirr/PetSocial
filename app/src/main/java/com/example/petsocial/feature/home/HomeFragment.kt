75e9d51fb13ac1bb0f0f398f9fac7680cd0a5994
volkan kelleci <kellecivolkan@gmail.com>
2024-01-22 20:45:42 +0300

    Added, Adapter injected

--- app/src/main/java/com/example/petsocial/feature/home/HomeFragment.kt
+++ app/src/main/java/com/example/petsocial/feature/home/HomeFragment.kt
@@ -1,19 +1,22 @@
 package com.example.petsocial.feature.home
 
-/**/
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.petsocial.R
 import com.example.petsocial.common.BaseViewBindingFragment
 import com.example.petsocial.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class HomeFragment : BaseViewBindingFragment<FragmentHomeBinding>() {
-
+    @Inject
+    lateinit var homeRecyclerAdapter: HomeRecyclerAdapter
     override fun inflateBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
@@ -26,7 +29,11 @@ class HomeFragment : BaseViewBindingFragment<FragmentHomeBinding>() {
         binding.fab.setOnClickListener{
             findNavController().navigate(R.id.action_homeFragment_to_postFragment)
         }
+        binding.usersHomeFragmentRecycler.adapter=homeRecyclerAdapter
+        binding.usersHomeFragmentRecycler.layoutManager=LinearLayoutManager(requireContext())
+        
     }
+
 
 
 }
