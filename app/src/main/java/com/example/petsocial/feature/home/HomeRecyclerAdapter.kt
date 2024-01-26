b5969b225efa38ef23af59a5b51430fc3663179d
volkan kelleci <kellecivolkan@gmail.com>
2024-01-26 09:31:48 +0300

    ADDED,Post data

--- app/src/main/java/com/example/petsocial/feature/home/HomeRecyclerAdapter.kt
+++ app/src/main/java/com/example/petsocial/feature/home/HomeRecyclerAdapter.kt
@@ -8,33 +8,31 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.RequestManager
 import com.example.petsocial.databinding.HomePostRawBinding
-import com.example.petsocial.feature.post.PostData
 import javax.inject.Inject
 
 class
 HomeRecyclerAdapter @Inject constructor(
-    val glide:RequestManager
-): RecyclerView.Adapter<HomeRecyclerAdapter.HomeAdapterVH>() {
-    class HomeAdapterVH(binding:HomePostRawBinding):RecyclerView.ViewHolder(binding.root)
+    val glide: RequestManager
+) : RecyclerView.Adapter<HomeRecyclerAdapter.HomeAdapterVH>() {
+    class HomeAdapterVH(binding: HomePostRawBinding) : RecyclerView.ViewHolder(binding.root)
 
-    private val diffUtil=object : DiffUtil.ItemCallback<PostData>(){
-        override fun areItemsTheSame(oldItem: PostData, newItem: PostData): Boolean {
-            return oldItem==newItem
+    private val diffUtil = object : DiffUtil.ItemCallback<HomeData>() {
+        override fun areItemsTheSame(oldItem: HomeData, newItem: HomeData): Boolean {
+            return oldItem == newItem
         }
 
         @SuppressLint("DiffUtilEquals")
-        override fun areContentsTheSame(oldItem: PostData, newItem: PostData): Boolean {
-            return oldItem==newItem
+        override fun areContentsTheSame(oldItem: HomeData, newItem: HomeData): Boolean {
+            return oldItem == newItem
         }
-
     }
-    private val recyclerListDiffer=AsyncListDiffer(this@HomeRecyclerAdapter,diffUtil)
-    var posts:List<PostData>
-        get()=recyclerListDiffer.currentList
+    private val recyclerListDiffer = AsyncListDiffer(this@HomeRecyclerAdapter, diffUtil)
+    var posts: List<HomeData>
+        get() = recyclerListDiffer.currentList
         set(value) = recyclerListDiffer.submitList(value)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeAdapterVH {
-        val view= HomePostRawBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        val view = HomePostRawBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return HomeAdapterVH(view)
     }
 
@@ -44,12 +42,12 @@ HomeRecyclerAdapter @Inject constructor(
 
     override fun onBindViewHolder(holder: HomeAdapterVH, position: Int) {
         val binding = HomePostRawBinding.bind(holder.itemView)
-        val imageView=binding.postRawImage
-        val postTitle=binding.postTitle
-        val post=posts[position]
+        val imageView = binding.postRawImage
+        val postTitle = binding.postTitle
+        val post = posts[position]
         binding.apply {
-            glide.load(post.postPhoto).into(imageView)
-            postTitle.text=post.postTitlte
+            glide.load(post.postImage).into(imageView)
+            postTitle.text = post.title
         }
 
     }
