cdc41fa412dcad7de1a8d263c533a33e0acf30b3
volkan kelleci <kellecivolkan@gmail.com>
2024-01-28 18:58:10 +0300

    Added, Uri list

--- app/src/main/java/com/example/petsocial/feature/post/PostFragment.kt
+++ app/src/main/java/com/example/petsocial/feature/post/PostFragment.kt
@@ -3,6 +3,7 @@ package com.example.petsocial.feature.post
 import android.Manifest
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
@@ -22,28 +23,30 @@ import com.bumptech.glide.RequestManager
 import com.example.petsocial.R
 import com.example.petsocial.common.BaseViewBindingFragment
 import com.example.petsocial.databinding.FragmentPostBinding
+import com.example.petsocial.util.Util
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.util.UUID
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class PostFragment @Inject constructor(private val glide: RequestManager)
-    :BaseViewBindingFragment<FragmentPostBinding>() {
+class PostFragment @Inject constructor(private val glide: RequestManager) :
+    BaseViewBindingFragment<FragmentPostBinding>() {
 
     private val viewModel: PostViewModel by viewModels()
-    private var picture:String=""
-    private var animalType:String=""
-    private var animalEstrusPeriod:String=""
-    private var geniusInfo:String=""
-    private var vaccationInfo:String=""
+    private var animalType: String = ""
+    private var animalEstrusPeriod: String = ""
+    private var geniusInfo: String = ""
+    private var vaccationInfo: String = ""
+    private var uriList = mutableListOf<Uri>()
 
     override fun inflateBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentPostBinding {
-        return FragmentPostBinding.inflate(inflater,container,false)
+        return FragmentPostBinding.inflate(inflater, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -54,20 +57,34 @@ class PostFragment @Inject constructor(private val glide: RequestManager)
         estrusRadioGroupChange()
         binding.createPostButton.setOnClickListener {
             initObserve()
-            val ageInfo=binding.ageEditText.text.toString()
-            val title=binding.title.text.toString()
-            val postData=PostData(picture,animalType,geniusInfo,ageInfo,vaccationInfo,animalEstrusPeriod,title)
-            postSave(postData)
+
+            val id = UUID.randomUUID()
+            val ageInfo = binding.ageEditText.text.toString()
+            val title = binding.title.text.toString()
+            val postData = PostData(
+                id.toString(),
+                Util.auth.currentUser!!.uid,
+                animalType,
+                geniusInfo,
+                ageInfo,
+                vaccationInfo,
+                animalEstrusPeriod,
+                title,
+            )
+
+            postSave(postData, uriList)
         }
     }
-    private fun postSave(postData:PostData){
-        viewModel.postSave(postData)
+
+    private fun postSave(postData: PostData,
+                          files : List<Uri>) {
+        viewModel.postSave(postData, files)
         initObserve()
     }
 
 
-    private fun showCatGeniusList(){
-        lifecycleScope.launch(Dispatchers.Main){
+    private fun showCatGeniusList() {
+        lifecycleScope.launch(Dispatchers.Main) {
             viewModel.catGeniusOptions.collect {
                 updateGeniusSpinner(it)
             }
@@ -98,44 +115,50 @@ class PostFragment @Inject constructor(private val glide: RequestManager)
         val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, options)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         binding.geniusInfoSpinner.adapter = adapter
-        binding.geniusInfoSpinner.onItemSelectedListener=object :AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                val selectedGenius = p0?.getItemAtPosition(p2).toString()
-                geniusInfo=selectedGenius
-            }
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-                TODO("Not yet implemented")
-            }
+        binding.geniusInfoSpinner.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                    val selectedGenius = p0?.getItemAtPosition(p2).toString()
+                    geniusInfo = selectedGenius
+                }
 
-        }
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+
+            }
     }
+
     private fun updateVaccinationSpinner(options: List<String>) {
         val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, options)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         binding.vaccinationSpinner.adapter = adapter
-        binding.vaccinationSpinner.onItemSelectedListener=object :AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                val selectedVac = p0?.getItemAtPosition(p2).toString()
-                vaccationInfo=selectedVac
-            }
+        binding.vaccinationSpinner.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                    val selectedVac = p0?.getItemAtPosition(p2).toString()
+                    vaccationInfo = selectedVac
+                }
 
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-                TODO("Not yet implemented")
-            }
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
 
-        }
+            }
     }
+
     private fun animalRadioGroupChange(): View? {
         binding.animalTypeRadioGroup.setOnCheckedChangeListener { _, checkedId ->
             // checkedId, seçilen RadioButton'ın ID'sidir
             val selectedRadioButton: RadioButton = requireActivity().findViewById(checkedId)
             val selectedAnimalType: String = selectedRadioButton.text.toString()
-            animalType=selectedAnimalType
+            animalType = selectedAnimalType
             when (checkedId) {
                 R.id.catRadioButton -> {
                     showCatGeniusList()
                     showCatVacList()
                 }
+
                 R.id.dogRadioButton -> {
                     showDogGeniusList()
                     showDogVacList()
@@ -144,16 +167,18 @@ class PostFragment @Inject constructor(private val glide: RequestManager)
         }
         return view
     }
+
     private fun estrusRadioGroupChange(): View? {
         binding.animalsEstrusPeriod.setOnCheckedChangeListener { _, checkedId ->
             // checkedId, seçilen RadioButton'ın ID'sidir
             val selectedRadioButton: RadioButton = requireActivity().findViewById(checkedId)
             val selectedEstrusPeriod: String = selectedRadioButton.text.toString()
-            animalEstrusPeriod=selectedEstrusPeriod
+            animalEstrusPeriod = selectedEstrusPeriod
             when (checkedId) {
                 R.id.yesRadio -> {
                     // Kedi seçildiğinde yapılacak işlemler
                 }
+
                 R.id.noRadio -> {
                     // Köpek seçildiğinde yapılacak işlemler
                 }
@@ -161,16 +186,20 @@ class PostFragment @Inject constructor(private val glide: RequestManager)
         }
         return view
     }
-    private fun takesPermAndPhoto(){
-        val register=registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                // There are no request codes
-                val data: Intent? = result.data
-                data?.data
-                glide.load(data?.data).into(binding.selectedImage)
-                picture= data?.data.toString()
+
+    private fun takesPermAndPhoto() {
+        val register =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    // There are no request codes
+                    val data: Intent? = result.data
+                    if (data != null && data.data != null){
+
+                        uriList.add(data.data!!)
+                        glide.load(data.data).into(binding.selectedImage)
+                    }
+                }
             }
-        }
         val requestPermissionLauncher = registerForActivityResult(
             ActivityResultContracts.RequestPermission()
         ) { isGranted ->
@@ -178,7 +207,8 @@ class PostFragment @Inject constructor(private val glide: RequestManager)
                 val gallery = Intent(MediaStore.ACTION_PICK_IMAGES)
                 gallery.setDataAndType(
                     MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                    "image/*")
+                    "image/*"
+                )
                 register.launch(gallery)
 
             } else {
@@ -197,36 +227,40 @@ class PostFragment @Inject constructor(private val glide: RequestManager)
     }
     private fun initObserve(){
         CoroutineScope(Dispatchers.Main).launch {
-            viewModel.postSuccess.collect{success->
+            viewModel.postSuccess.collect { success ->
                 success?.let {
-                    if (success){
+                    if (success) {
                         findNavController().popBackStack()
-                    }else{
-                        Toast.makeText(context, "Please Fill All Information Trust", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(
+                            context,
+                            "Please Fill All Information Trust",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
                 }
             }
         }
         CoroutineScope(Dispatchers.Main).launch {
-            viewModel.loadingState.collect{loading->
+            viewModel.loadingState.collect { loading ->
                 loading?.let {
-                    if (loading){
-                        binding.constraintOfPost.visibility=View.INVISIBLE
-                        binding.progressBar.visibility=View.VISIBLE
-                    }else{
-                        binding.constraintOfPost.visibility=View.VISIBLE
-                        binding.progressBar.visibility=View.GONE
+                    if (loading) {
+                        binding.constraintOfPost.visibility = View.INVISIBLE
+                        binding.progressBar.visibility = View.VISIBLE
+                    } else {
+                        binding.constraintOfPost.visibility = View.VISIBLE
+                        binding.progressBar.visibility = View.GONE
                     }
                 }
 
             }
         }
         CoroutineScope(Dispatchers.Main).launch {
-            viewModel.errorString.collect{error->
+            viewModel.errorString.collect { error ->
                 error?.let {
                     Toast.makeText(context, error, Toast.LENGTH_SHORT).show()
                 }
             }
         }
     }
-    }
+}
