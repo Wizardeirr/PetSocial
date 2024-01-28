56d65bb1c38990f06eae1d1a876855ab1633230b
volkan kelleci <kellecivolkan@gmail.com>
2024-01-28 18:57:00 +0300

    ADDED,Storage ref to adapter

--- app/src/main/java/com/example/petsocial/feature/home/HomeRecyclerAdapter.kt
+++ app/src/main/java/com/example/petsocial/feature/home/HomeRecyclerAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.petsocial.feature.home
 
 import android.annotation.SuppressLint
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.AsyncListDiffer
@@ -8,6 +9,7 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.RequestManager
 import com.example.petsocial.databinding.HomePostRawBinding
+import com.google.firebase.storage.FirebaseStorage
 import javax.inject.Inject
 
 class
@@ -45,8 +47,26 @@ HomeRecyclerAdapter @Inject constructor(
         val imageView = binding.postRawImage
         val postTitle = binding.postTitle
         val post = posts[position]
+
         binding.apply {
-            glide.load(post.postImage).into(imageView)
+            val storageReference = FirebaseStorage.getInstance().reference.child("posts").child(post.postImage)
+            storageReference.downloadUrl
+                .addOnSuccessListener { uri ->
+                    // URI başarıyla alındı, bu URI'yi kullanabilirsiniz
+                    val downloadUrl = uri.toString()
+
+                    // Şimdi bu URL'yi istediğiniz şekilde kullanabilirsiniz
+                    // Örneğin, Glide ile ImageView'e resmi yükleyebilirsiniz
+                    glide
+                        .load(downloadUrl)
+                        .into(imageView)
+                }
+                .addOnFailureListener { exception ->
+                    // URI alınırken bir hata oluştu
+                    Log.d("Storage", "Error getting download URL", exception)
+                }
+                glide.load(storageReference).into(imageView)
+
             postTitle.text = post.title
         }
 
