ed1716e7d31403c01466e96f36f88ed21ca8c3fe
volkan kelleci <kellecivolkan@gmail.com>
2024-02-02 21:59:40 +0300

    Added, instance created for when need context

--- app/src/main/java/com/example/petsocial/PetSocialApplication.kt
+++ app/src/main/java/com/example/petsocial/PetSocialApplication.kt
@@ -1,13 +1,16 @@
 package com.example.petsocial
 
 import android.app.Application
-import com.google.firebase.FirebaseApp
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class PetSocialApplication:Application() {
+    companion object{
+        lateinit var instance:PetSocialApplication
+    }
     override fun onCreate() {
         super.onCreate()
+        instance=this
 
     }
 }
